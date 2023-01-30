bayesian softmax loss is fixed

keras_retinanet/bin/train.py
@@ -133,9 +133,9 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
     sigma_sq_smooth_l1 = training_model.add_weight(dtype=tf.float32, name='sigma_sq_smooth_l1',
                                                   initializer=tf.constant_initializer(1.0),
                                                   trainable=True)                            
-    # regression_loss, loss_class_reg = losses.smooth_l1(sigma_var=sigma_sq_smooth_l1)
-    regression_loss, loss_class_reg, running_mean, running_var = losses.adjust_smooth_l1( 
-                                        weights=[running_mean_smooth_l1, running_var_smooth_l1, sigma_sq_smooth_l1])
+    regression_loss, loss_class_reg = losses.smooth_l1(sigma_var=sigma_sq_smooth_l1)
+    # regression_loss, loss_class_reg, running_mean, running_var = losses.adjust_smooth_l1( 
+    #                                     weights=[running_mean_smooth_l1, running_var_smooth_l1, sigma_sq_smooth_l1])
 
 
     classification_loss, loss_class_cl = losses.focal(sigma_var=sigma_sq_focal)
@@ -159,7 +159,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
         optimizer=keras.optimizers.Adam(lr=lr, clipnorm=optimizer_clipnorm)
         # metrics=[np.float(running_mean), np.float(running_var)]
     )
-
+    running_mean, running_var = [0], [0]
     return model, training_model, prediction_model, [loss_class_reg, loss_class_cl], [running_mean, running_var]
 
 

keras_retinanet/losses.py
@@ -67,16 +67,17 @@ def focal(alpha=0.25, gamma=2.0, cutoff=0.5, sigma_var=None):
         #                             -0.5 * sigma_var))
         
         #from Alexey Lapenok master thesis:
-        # focal_weight = tf.where(keras.backend.equal(labels, 1),
-        #                              1 - classification ** keras.backend.exp(-sigma_var)* keras.backend.exp(
-        #                             -0.5 * sigma_var),
-        #                              1 - (1 - classification) ** keras.backend.exp(-sigma_var)* keras.backend.exp(
-        #                             -0.5 * sigma_var))
-        #try then
         focal_weight = tf.where(keras.backend.equal(labels, 1),
-                                     1 - classification * keras.backend.exp(-1.5 * keras.backend.pow(sigma_var, 2)),
-                                     classification * keras.backend.exp(
-                                         -1.5 * keras.backend.pow(sigma_var, 2)))       
+                                     1 - classification ** keras.backend.exp(-sigma_var) * keras.backend.exp(
+                                    -0.5 * sigma_var),
+                                     classification ** keras.backend.exp(-sigma_var) * keras.backend.exp(
+                                    -0.5 * sigma_var))
+        #try then
+        # focal_weight = tf.where(keras.backend.equal(labels, 1),
+        #                              1 - classification * keras.backend.exp(-1.5 * keras.backend.pow(sigma_var, 2)),
+        #                              classification * keras.backend.exp(
+        #                                  -1.5 * keras.backend.pow(sigma_var, 2)))       
+     
         focal_weight = alpha_factor * focal_weight ** gamma
 
         cross_entropy = keras.backend.binary_crossentropy(labels, classification) * keras.backend.exp(

keras_retinanet/utils/visualization.py
@@ -22,7 +22,7 @@ import csv
 from .colors import label_color
 
 
-def draw_box(image, box, color, thickness=2):
+def draw_box(image, box, color, name='0', i=0, thickness=2, save_path="."):
     """ Draws a box on an image with a given color.
 
     # Arguments
@@ -33,6 +33,9 @@ def draw_box(image, box, color, thickness=2):
     """
     b = np.array(box).astype(int)
     cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
+
+    cropped_image = image[b[1]:b[3], b[0]:b[2]]
+    cv2.imwrite(os.path.join(save_path, '{}{}.png'.format(name, i)), cropped_image)
 
 
 def crop_box(image, name, annotations, label_to_name=None, save_path=".", group=False):